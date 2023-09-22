2cc9a071c581ce885d9773929697805c1249785b
AffanShaikh100 <133389464+AffanShaikh100@users.noreply.github.com>
2023-09-22 23:02:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+
+let target = document.getElemntById("level");
+
+let level = 1;
+
+while(true){
+
+	if(target.parentNode == "HTML"){
+		break;
+	}
+	target = target.parentNode;
+	level++;
+}
+
+alert(`fbjbfajkb`);
